c3a318c541e1762eb79df334e73192f04da5e15a
Vladislav Sorokin <sorokinvj@gmail.com>
2017-12-31 02:31:15 +0300

    много всего сразу,сорри

---
+++ js/newyear.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+function surprise(cb) {
+    (function loop() {
+        var now = new Date();
+        if (now.getDate() === 1 && now.getHours() === 0 && now.getMinutes() === 0 && now.getSeconds() === 0) {
+            $("#bgvid")[0].load();
+        }
+        now = new Date();                  // allow for time passing
+        var delay = 60000 - (now % 60000); // exact ms to next minute interval
+        setTimeout(loop, delay);
+        console.log(now);
+    })();
+}
+
+function changevideo() {
+  var videoFile = 'nwy.mp4';
+  $('#bgvid source').attr('src', videoFile);
+  console.log('HAPPENED!!!')
+}
+surprise(changevideo());
+});
