b9d8ffca7152d3e7f326306a9519b965d264f5aa
Vladislav Sorokin <sorokinvj@gmail.com>
2017-12-31 17:51:33 +0300

    прелоадер

--- js/newyear.js
+++ js/newyear.js
@@ -1,21 +1,36 @@
 
 $(document).ready(function(){
+
   $.html5Loader({
-        filesToLoad:    ' http://newyearwithoutputin.com/files.json', // this could be a JSON or simply a javascript object
-        onBeforeLoad:       function () {},
-        onComplete:         function () {console.log("hooray, files loaded!!!")},
+        filesToLoad:       {
+            "files": [
+              {
+                "type":"VIDEO",
+                "sources": {
+                    "h264":{
+                      "source":"http://newyearwithoutputin.com/nwy2.mp4",
+                      "size":73163519
+                    }
+                  }
+                }
+            ]
+          }, // this could be a JSON or simply a javascript object
+        onBeforeLoad:       function () {console.log("going to load");},
+        onComplete:         function () {console.log("hooray, files loaded!!!");},
         onElementLoaded:    function ( obj, elm) { },
-        onUpdate:           function ( percentage ) {}
+        onUpdate:           function ( percentage ) {console.log('loaded: ', percentage);}
   });
 
 function surprise() {
     (function loop() {
         var now = new Date();
         if (now.getDate() === 1 && now.getHours() === 0 && now.getMinutes() === 0 && now.getSeconds() === 0) {
-            var videoFile = 'nwy2.mp4';
-            $('#bgvid source').attr('src', videoFile);
-            $("#bgvid")[0].load();
-            console.log('HAPPENED!!!')
+          var videoFile = 'nwy2.mp4';
+          $('#bgvid source').attr('src', videoFile);
+          $("#bgvid")[0].load();
+          console.log('HAPPENED!!!');
+          $('#my-soon-counter').addClass('inactive');
+          $('#deezer_wrapper').addClass('inactive');
         }
         now = new Date();                  // allow for time passing
         var delay = 60000 - (now % 60000); // exact ms to next minute interval
